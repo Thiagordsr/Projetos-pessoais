286a6fd9b9fce42617401ddd152445df637f4b53
Thiagordsr <thiagorodriguessantarosa19@gmail.com>
2026-07-19 04:07:59 -0300

    alteraçao

---
+++ quizFutebol.py
@@ -0,0 +1,87 @@
+print ("            --------------------------\nVAMOS VER QUAL JOGADOR DO BRASILEIRAO 2026 VOCE É \n       quiz brasileirao 2026      \n       responda as perguntas       \n           --------------------------")
+
+nota = float(input("digite de 0 a 10 o quanto vc se considera bom no futebol: "))
+
+
+print ("pra qual time voce torce? \n1. CORINTHIANS\n2. SANTOS\n3. PALMEIRAS\n4. CRUZEIRO\n5. SAO PAULO\n6. FLAMENGO\n7. INTERNACIONAL\n8. VASCO\n ")
+
+time =  int(input("digite o numero: "))
+
+
+if time == 1:
+    print("VAAAAAAAIIII CORINTHIANSSS")
+elif time == 2:
+    print("PEIXAAUMMMM HMM") 
+elif time == 3:
+    print("palmeirense kkk") 
+elif time == 4:
+    print("MARIA KKKKKKKK") 
+elif time == 5:
+    print("UI SAO PAULINOOO HMMMMMMM") 
+elif time == 6:
+    print("flamenguista? qual o seu segundo time ? KKKK") 
+elif time == 7:
+    print("internacional tche")
+elif time == 8:
+    print("vascaaumm")
+else:
+    print("ESCOLHA UM DESSES CARAI")
+
+print("com qual perna vc chuta? \nDIREITA: 1 \nESQUERDA: 2\n")
+chute = int(input ("--> "))
+
+if chute == 1:
+    print("vc é comum")
+else:
+    print("eitaaa canhotinha")
+
+print("_________________________________________________________________\n")
+
+if nota >= 1000:
+    print("voce nao é tudo isso nao amigao")
+
+
+elif nota == 10:
+    print ("CAÇAAAA RATOOOOOOOOOOOOOO")
+
+elif nota >= 9.5:
+    print("gustavo gomis do varmeiras")
+
+elif nota >= 9.0:
+    print("fraco lopes do parmeiras")
+
+elif nota >= 8.5:
+    print("arrascarreta do varmengo")
+
+elif nota >= 8.0:
+    print("memphis depao")
+
+elif nota >= 7.5:
+    print("marcos antonio do gay paulo")
+
+elif nota >= 7.0:
+    print("kaio jorge do cruzeiro")
+
+elif nota >= 6.5:
+    print("pulgar do flamemgo")
+
+elif nota >= 6.0:
+    print("gui negao do corinthians")
+
+elif nota >= 5.0:
+    print("ze ivaldo ")
+
+elif nota >= 4.0:
+    print("ribamar e olhe la KKKKK")
+
+elif nota >= 3.0:
+    print("saboorr jogador")
+
+elif nota >= 2.0:
+    print("nem reserva no fut da rua")
+
+elif nota >= 1.0:
+    print("seu lugar deve ser na arquibancada gritando e torcendo")
+
+else:
+    print("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK\nKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK\nKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK")
